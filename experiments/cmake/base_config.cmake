e29b19ef6c2a0f2b28f2c78740d41ac2f88d5b09
studyztp <studyztp@gmail.com>
2025-02-20 01:00:06 +0000

    add c-hook tool param in experiment config

--- experiments/cmake/base_config.cmake
+++ experiments/cmake/base_config.cmake
@@ -4,6 +4,16 @@ set(NUGGET_LIBRARY_PATH "${NUGGET_UTIL_PATH}/cmake")
 set(NUGGET_HOOKS_PATH "${NUGGET_UTIL_PATH}/hook_helper")
 set(NUGGET_C_HOOKS_PATH "${NUGGET_HOOKS_PATH}/c_hooks")
 
+set(PAPI_PATH "${NUGGET_HOOKS_PATH}/other_tools/papi/aarch64")
+set(M5_PATH "${NUGGET_HOOKS_PATH}/other_tools/gem5/arm64")
+set(M5_INCLUDE_PATH "${NUGGET_HOOKS_PATH}/other_tools/gem5/include")
+
+# set if we should use addr mop m5 ops in the beginning and end of the ROI
+# for
+set(IF_USE_ADDR_VERSION_M5OPS_BEGIN TRUE)
+set(IF_USE_ADDR_VERSION_M5OPS_END TRUE)
+
+
 set(LLVM_ROOT "/home/ztpc/compiler/llvm-dir")
 set(LLVM_BIN "${LLVM_ROOT}/bin")
 
