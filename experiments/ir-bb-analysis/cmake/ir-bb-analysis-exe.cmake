a243ad7b22b98dbd77945144a05bf169f477b92e
studyztp <studyztp@gmail.com>
2025-02-19 23:59:46 -0800

    update submodules; update llc commands to config.cmake

--- experiments/ir-bb-analysis/cmake/ir-bb-analysis-exe.cmake
+++ experiments/ir-bb-analysis/cmake/ir-bb-analysis-exe.cmake
@@ -1,8 +1,24 @@
 list(APPEND CMAKE_MODULE_PATH "${CMAKE_CURRENT_LIST_DIR}/../../cmake")
 include(base_config)
 
+list(APPEND CMAKE_MODULE_PATH "${NUGGET_LIBRARY_PATH}")
+include(Nugget)
+
 set(LLC_EXTRATION_FILE_PATH
     ${NUGGET_UTIL_PATH}/cmake/check-cpu-features/llc-command.txt)
+
+
+if(LLC_EXTRATION_FILE_PATH AND EXISTS ${LLC_EXTRATION_FILE_PATH})
+    nugget_read_list(LLC_CMD ${LLC_EXTRATION_FILE_PATH})
+else()
+    message(WARNING "LLC command not found in ${LLC_EXTRATION_FILE_PATH}")
+endif()
+
+if(LLC_CMD)
+    list(APPEND LLC_CMD -relocation-model=pic -O3)
+endif()
+
+message(STATUS "LLC_CMD: ${LLC_CMD}")
 
 set(ANALYSIS_BC_FILE_PATH
     "${CMAKE_CURRENT_BINARY_DIR}/llvm-bc/lsms_ir_bb_analysis_bc/lsms_ir_bb_analysis_bc.bc")
--- experiments/papi-nugget/cmake/papi-nugget-exe.cmake
+++ experiments/papi-nugget/cmake/papi-nugget-exe.cmake
@@ -1,6 +1,9 @@
 list(APPEND CMAKE_MODULE_PATH "${CMAKE_CURRENT_LIST_DIR}/../../cmake")
 include(base_config)
 
+list(APPEND CMAKE_MODULE_PATH "${NUGGET_LIBRARY_PATH}")
+include(Nugget)
+
 set(TARGET_NAME "papi_nugget_exe")
 set(HOOK_TARGET single-threaded-papi-nugget)
 
@@ -14,3 +17,15 @@ set(NUGGET_BC_FILE_BASENAME
 
 set(LLC_EXTRATION_FILE_PATH
     "${NUGGET_UTIL_PATH}/cmake/check-cpu-features/llc-command.txt")
+
+    if(LLC_EXTRATION_FILE_PATH AND EXISTS ${LLC_EXTRATION_FILE_PATH})
+    nugget_read_list(LLC_CMD ${LLC_EXTRATION_FILE_PATH})
+else()
+    message(WARNING "LLC command not found in ${LLC_EXTRATION_FILE_PATH}")
+endif()
+
+if(LLC_CMD)
+    list(APPEND LLC_CMD -relocation-model=pic -O3)
+endif()
+
+message(STATUS "LLC_CMD: ${LLC_CMD}")
