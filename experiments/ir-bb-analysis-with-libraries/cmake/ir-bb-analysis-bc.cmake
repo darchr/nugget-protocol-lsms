95f799bb9615421ca2aaa2c17ec76abb91f059cb
studyztp <studyztp@gmail.com>
2025-02-23 18:07:23 -0800

    update base on nugget library update

--- experiments/ir-bb-analysis-with-libraries/cmake/ir-bb-analysis-bc.cmake
+++ experiments/ir-bb-analysis-with-libraries/cmake/ir-bb-analysis-bc.cmake
@@ -5,6 +5,6 @@ set(REGION_LENGTH 100000000)
 set(TARGET_NAME lsms_ir_bb_analysis_include_library_bc)
 set(INCLUDE_LIBRARIES_IN_BC true)
 
-list(APPEND EXTRA_FLAGS "-D_longjmp=longjmp" "-D_POSIX_C_SOURCE=200809L")
+list(APPEND EXTRA_FLAGS "-fPIC" "-D_longjmp=longjmp" "-D_POSIX_C_SOURCE=200809L")
 
 set(BB_INFO_OUTPUT_DIR ${CMAKE_BINARY_DIR}/ir-bb-analysis-include-library-bb-info)
